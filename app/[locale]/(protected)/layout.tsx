bd569029d8c235e032266cf73f12640f5cc493e4
mitantsoa1 <ainamitantsogit@gmail.com>
2025-12-11 12:47:38 +0300

    redirect client

--- app/[locale]/(protected)/layout.tsx
+++ app/[locale]/(protected)/layout.tsx
@@ -1,16 +1,19 @@
+"use client"
 import { authClient } from "@/lib/auth-client";
 import { getClientSession, getSession } from "@/lib/get-session";
-import { Metadata } from "next";
-import { getLocale } from "next-intl/server";
-import { redirect } from "next/navigation";
+import { useLocale } from "next-intl";
+import { useRouter } from "next/navigation";
+// import { Metadata } from "next";
+// import { getLocale } from "next-intl/server";
+// import { redirect } from "next/navigation";
 import React from "react";
 
-export const metadata: Metadata = {
-    title: {
-        template: '%s - Dashboard | GNS BTP', // Le %s sera remplacé par le titre de la page
-        default: 'GNS BTP - Maîtriser l\'Excellence Gros Œuvre Bâtir', // Titre par défaut
-    },
-};
+// export const metadata: Metadata = {
+//     title: {
+//         template: '%s - Dashboard | GNS BTP', // Le %s sera remplacé par le titre de la page
+//         default: 'GNS BTP - Maîtriser l\'Excellence Gros Œuvre Bâtir', // Titre par défaut
+//     },
+// };
 
 
 /**
@@ -24,16 +27,28 @@ export default async function ProtectedLayout({
 }) {
     // Vérifier la session utilisateur
     // const session = await getSession();
-    const session = getClientSession();
     // console.log("Session utilisateur serverr:", session);
-    const locale = await getLocale();
+    // const locale = await getLocale();
 
     // const users = await authClient.useSession();
     // console.log("Session utilisateur client:", users);
 
     // Rediriger vers la page de connexion si non authentifié
+    // if (!session.data?.user) {
+    //     redirect(`/${locale}/login`);
+    // }
+
+
+
+    // client
+
+    const locale = useLocale()
+    const router = useRouter()
+
+    const session = authClient.useSession();
+
     if (!session.data?.user) {
-        redirect(`/${locale}/login`);
+        router.push(`${locale}/login`)
     }
 
     return <>{children}</>;
