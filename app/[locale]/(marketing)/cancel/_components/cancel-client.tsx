8531a1ea9be7d4e5f4e8a6c5b0d961c6dccc686b
mitantsoa1 <ainamitantsogit@gmail.com>
2025-12-12 15:28:21 +0300

    preprod

--- app/[locale]/(marketing)/cancel/_components/cancel-client.tsx
+++ app/[locale]/(marketing)/cancel/_components/cancel-client.tsx
@@ -1,17 +1,18 @@
 "use client";
-import { useTranslations } from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
 import Link from 'next/link';
-import ButtonText from '@/components/ButtonText';
+import ButtonPrimary from '@/components/ButtonPrimary';
 
 const CancelClientPage = () => {
     const t = useTranslations('cancel');
+    const locale = useLocale()
     return (
         <div className="bg-[#eee9ed] m-0 pt-32 text-center min-h-screen">
             <section className="max-w-4xl mx-auto px-4 pb-10">
                 <h1 className="text-4xl font-bold mb-4">{t('title')}</h1>
                 <p className="mb-8">{t('message')}</p>
-                <Link href="/pricing">
-                    <ButtonText>{t('cta')}</ButtonText>
+                <Link href={`/${locale}/pricing`}>
+                    <ButtonPrimary text={t(' cta')} />
                 </Link>
             </section>
         </div>
